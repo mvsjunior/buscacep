d7f4b83b67dc8d72e15e23aa42dd84afb965ad2c
Mauro Vieira <mauro.emc@outlook.com>
2022-04-18 02:26:52 -0300

    Commit Inicial

--- dist/app.js
+++ dist/app.js
@@ -3,9 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const helmet_1 = __importDefault(require("helmet"));
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
+const dotenv_1 = __importDefault(require("dotenv"));
 const mustache_express_1 = __importDefault(require("mustache-express"));
+const https_1 = __importDefault(require("https"));
+const fs_1 = __importDefault(require("fs"));
 const index_1 = __importDefault(require("./routes/index"));
 const app = (0, express_1.default)();
 app.set('view engine', 'mustache');
@@ -16,4 +20,14 @@ app.use(index_1.default);
 app.use((req, res) => {
     res.send("Página não encontrada");
 });
-app.listen(80);
+app.use((0, helmet_1.default)());
+dotenv_1.default.config();
+let port = process.env.PORT || 3000;
+https_1.default
+    .createServer({
+    key: fs_1.default.readFileSync(path_1.default.join(__dirname, "key.pem")),
+    cert: fs_1.default.readFileSync(path_1.default.join(__dirname, "cert.pem"))
+}, app)
+    .listen(port, () => {
+    console.log('server is runing at port ' + port);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
+import helmet from 'helmet';
 import express,{Request, Response} from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
+import https from 'https';
+import fs from 'fs';
 import mainRouter from './routes/index';
-
 
 const app = express();
 
@@ -30,5 +32,23 @@ app.use((req: Request, res: Response) => {
     res.send("Página não encontrada");
 });
 
+// ======================
+//  Helmet
+app.use(helmet());
 
-app.listen(80);
+// ======================
+//   HTTPS
+dotenv.config();
+
+let port = process.env.PORT || 3000 ;
+
+https
+  .createServer(
+      {
+          key: fs.readFileSync(path.join(__dirname, process.env.CERT_KEY_FILE)), 
+          cert: fs.readFileSync(path.join(__dirname, process.env.CERT_FILE))
+        },
+        app )
+        .listen(port, ()=>{
+            console.log('server is runing at port ' + port)
+        });
